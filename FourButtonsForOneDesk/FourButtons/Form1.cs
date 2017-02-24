c4edf587d620e03426a6a010ab4d0624ea4a5c2e
wenyinghan <j7pl14@yahoo.com.tw>
2017-02-24 15:17:39 +0800

    溫穎翰2

--- FourButtonsForOneDesk/FourButtons/Form1.cs
+++ FourButtonsForOneDesk/FourButtons/Form1.cs
@@ -21,5 +21,10 @@ namespace FourButtons
         {
             button1.Text = "溫穎翰";
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = "溫穎翰2";
+        }
     }
 }
